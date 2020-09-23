3dd01545fbc7d51311fb480566bcb31bb9b86c0b
Chris Smith <cs24@sanger.ac.uk>
2020-09-23 11:58:40 +0100

    Remove the fake dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
         <Route path="/">
           <Login />
         </Route>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
   return (
@@ -57,8 +56,8 @@ const Login: React.FC = () => {
           </div>
 
           <div className="mt-6">
-            <Link
-              to="/dashboard"
+            <button
+              onClick={() => alert("Coming Soon!")}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
@@ -75,7 +74,7 @@ const Login: React.FC = () => {
                 </svg>
               </span>
               Sign in
-            </Link>
+            </button>
           </div>
         </form>
       </div>
