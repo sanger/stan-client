16232528a4aa0f349706664eec90b1727292f3f1
Chris Smith <cs24@sanger.ac.uk>
2020-12-10 17:48:09 +0000

    Remove unused imports

--- src/components/SectioningLayout.tsx
+++ src/components/SectioningLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Actor } from "xstate";
 import PinkButton from "./buttons/PinkButton";
 import { useActor } from "@xstate/react";
