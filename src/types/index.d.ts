f3ea53e8d523303dc8301ce99f9705fb4c945ffa
Chris Smith <cs24@sanger.ac.uk>
2021-04-21 15:26:14 +0100

    Add config file for env-dependent settings

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,6 @@
 import { worker } from "../mocks/browser";
-import { Labware } from "./graphql";
 import { graphql } from "msw";
+import { StanConfig } from "./stan";
 
 export type workerType = typeof worker;
 
@@ -9,6 +9,7 @@ export type graphqlType = typeof graphql;
 // so they can be accessed in both runtime and test suites.
 declare global {
   interface Window {
+    STAN_CONFIG?: StanConfig;
     msw: {
       worker: workerType;
       graphql: graphqlType;
