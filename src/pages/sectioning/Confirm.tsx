646abb2fe3d2aad8a4f805a45205986bc805d575
Chris Smith <cs24@sanger.ac.uk>
2021-07-20 14:16:42 +0100

    Fix commit

--- src/pages/sectioning/Confirm.tsx
+++ src/pages/sectioning/Confirm.tsx
@@ -1,115 +1,113 @@
-// import React, { useContext, useEffect } from "react";
-// import AppShell from "../../components/AppShell";
-// import PinkButton from "../../components/buttons/PinkButton";
-// import { groupBy } from "lodash";
-// import Warning from "../../components/notifications/Warning";
-// import { useScrollToRef } from "../../lib/hooks";
-// import ConfirmByLabwareType from "./ConfirmByLabwareType";
-// import { SectioningPageContext } from "../Sectioning";
-// import OperationCompleteModal from "../../components/modal/OperationCompleteModal";
-// import { reload } from "../../lib/sdk";
-// import { LabwareTypeName } from "../../types/stan";
-// import DataTable from "../../components/DataTable";
-// import columns from "../../components/dataTable/labwareColumns";
-//
-// function Confirm() {
-//   const model = useContext(SectioningPageContext)!;
-//   const [ref, scrollToRef] = useScrollToRef();
-//
-//   // Scroll to the top of the page when this component is first loaded
-//   useEffect(() => window.scrollTo(0, 0), []);
-//
-//   // When there's an error, make sure the page scrolls to it so it's in view
-//   useEffect(() => {
-//     if (model.isConfirmError) {
-//       scrollToRef();
-//     }
-//   }, [model, scrollToRef]);
-//
-//   const { sectioningLayouts } = model.context;
-//   const layoutPlans = sectioningLayouts.flatMap((sl) => sl.coreLayoutPlans);
-//
-//   const layoutPlansByLabwareType = groupBy(
-//     layoutPlans,
-//     (lp) => lp.destinationLabware.labwareType.name
-//   );
-//
-//   return (
-//     <AppShell>
-//       <AppShell.Header>
-//         <AppShell.Title>Sectioning - Summary</AppShell.Title>
-//       </AppShell.Header>
-//       <AppShell.Main>
-//         <div className="my-4 mx-auto max-w-screen-xl space-y-16">
-//           <DataTable
-//             data={model.context.sourceLabwares}
-//             columns={[columns.barcode(), columns.highestSectionForSlot("A1")]}
-//           />
-//
-//           <div className="space-y-4">
-//             {/* Always show tubes first (if there are any) */}
-//             {layoutPlansByLabwareType?.[LabwareTypeName.TUBE] && (
-//               <ConfirmByLabwareType
-//                 labwareTypeName={LabwareTypeName.TUBE}
-//                 layoutPlans={layoutPlansByLabwareType[LabwareTypeName.TUBE]}
-//               />
-//             )}
-//
-//             {/* Filter out tubes as they've been shown above */}
-//             {Object.entries(layoutPlansByLabwareType)
-//               .filter(
-//                 ([labwareTypeName, _]) =>
-//                   labwareTypeName !== LabwareTypeName.TUBE
-//               )
-//               .map(([labwareTypeName, lps], i) => (
-//                 <ConfirmByLabwareType
-//                   key={i}
-//                   layoutPlans={lps}
-//                   labwareTypeName={labwareTypeName}
-//                 />
-//               ))}
-//             {model.isConfirmError && (
-//               <div ref={ref}>
-//                 <Warning
-//                   error={model.context.serverErrors}
-//                   message={
-//                     "There was an error confirming the Sectioning operation"
-//                   }
-//                 />
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       </AppShell.Main>
-//
-//       <div className="border border-t-2 border-gray-200 w-full py-4 px-4 sm:px-6 lg:px-8 bg-gray-100 flex-shrink-0">
-//         <div className="max-w-screen-xl mx-auto">
-//           <div className="flex flex-row items-center justify-end">
-//             <PinkButton
-//               disabled={model.isDone}
-//               onClick={model.confirmOperation}
-//               action="primary"
-//             >
-//               Save
-//             </PinkButton>
-//           </div>
-//         </div>
-//       </div>
-//
-//       <OperationCompleteModal
-//         message={"Sectioning Saved"}
-//         show={model.isDone}
-//         onReset={reload}
-//       >
-//         <p>
-//           If you wish to start the process again, click the "Reset Form" button.
-//           Otherwise you can return to the Home screen.
-//         </p>
-//       </OperationCompleteModal>
-//     </AppShell>
-//   );
-// }
-//
-// export default Confirm;
+import React, { useContext, useEffect } from "react";
+import AppShell from "../../components/AppShell";
+import PinkButton from "../../components/buttons/PinkButton";
+import { groupBy } from "lodash";
+import Warning from "../../components/notifications/Warning";
+import { useScrollToRef } from "../../lib/hooks";
+import ConfirmByLabwareType from "./ConfirmByLabwareType";
+import { SectioningPageContext } from "../Sectioning";
+import OperationCompleteModal from "../../components/modal/OperationCompleteModal";
+import { reload } from "../../lib/sdk";
+import { LabwareTypeName } from "../../types/stan";
+import DataTable from "../../components/DataTable";
+import columns from "../../components/dataTable/labwareColumns";
 
-export {};
+function Confirm() {
+  const model = useContext(SectioningPageContext)!;
+  const [ref, scrollToRef] = useScrollToRef();
+
+  // Scroll to the top of the page when this component is first loaded
+  useEffect(() => window.scrollTo(0, 0), []);
+
+  // When there's an error, make sure the page scrolls to it so it's in view
+  useEffect(() => {
+    if (model.isConfirmError) {
+      scrollToRef();
+    }
+  }, [model, scrollToRef]);
+
+  const { sectioningLayouts } = model.context;
+  const layoutPlans = sectioningLayouts.flatMap((sl) => sl.coreLayoutPlans);
+
+  const layoutPlansByLabwareType = groupBy(
+    layoutPlans,
+    (lp) => lp.destinationLabware.labwareType.name
+  );
+
+  return (
+    <AppShell>
+      <AppShell.Header>
+        <AppShell.Title>Sectioning - Summary</AppShell.Title>
+      </AppShell.Header>
+      <AppShell.Main>
+        <div className="my-4 mx-auto max-w-screen-xl space-y-16">
+          <DataTable
+            data={model.context.sourceLabwares}
+            columns={[columns.barcode(), columns.highestSectionForSlot("A1")]}
+          />
+
+          <div className="space-y-4">
+            {/* Always show tubes first (if there are any) */}
+            {layoutPlansByLabwareType?.[LabwareTypeName.TUBE] && (
+              <ConfirmByLabwareType
+                labwareTypeName={LabwareTypeName.TUBE}
+                layoutPlans={layoutPlansByLabwareType[LabwareTypeName.TUBE]}
+              />
+            )}
+
+            {/* Filter out tubes as they've been shown above */}
+            {Object.entries(layoutPlansByLabwareType)
+              .filter(
+                ([labwareTypeName, _]) =>
+                  labwareTypeName !== LabwareTypeName.TUBE
+              )
+              .map(([labwareTypeName, lps], i) => (
+                <ConfirmByLabwareType
+                  key={i}
+                  layoutPlans={lps}
+                  labwareTypeName={labwareTypeName}
+                />
+              ))}
+            {model.isConfirmError && (
+              <div ref={ref}>
+                <Warning
+                  error={model.context.serverErrors}
+                  message={
+                    "There was an error confirming the Sectioning operation"
+                  }
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </AppShell.Main>
+
+      <div className="border border-t-2 border-gray-200 w-full py-4 px-4 sm:px-6 lg:px-8 bg-gray-100 flex-shrink-0">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="flex flex-row items-center justify-end">
+            <PinkButton
+              disabled={model.isDone}
+              onClick={model.confirmOperation}
+              action="primary"
+            >
+              Save
+            </PinkButton>
+          </div>
+        </div>
+      </div>
+
+      <OperationCompleteModal
+        message={"Sectioning Saved"}
+        show={model.isDone}
+        onReset={reload}
+      >
+        <p>
+          If you wish to start the process again, click the "Reset Form" button.
+          Otherwise you can return to the Home screen.
+        </p>
+      </OperationCompleteModal>
+    </AppShell>
+  );
+}
+
+export default Confirm;
--- src/pages/sectioning/ConfirmTubes.tsx
+++ src/pages/sectioning/ConfirmTubes.tsx
@@ -1,210 +1,209 @@
-export {};
-// import React, { useCallback, useContext, useEffect } from "react";
-// import { useMachine, useSelector } from "@xstate/react";
-// import classNames from "classnames";
-// import Table, {
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-// } from "../../components/Table";
-// import RemoveIcon from "../../components/icons/RemoveIcon";
-// import { createSectioningConfirmMachine } from "../../lib/machines/sectioning/sectioningConfirm/sectioningConfirmMachine";
-// import { LayoutPlan } from "../../lib/machines/layout/layoutContext";
-// import { SectioningPageContext } from "../Sectioning";
-// import {
-//   buildSlotColor,
-//   buildSlotSecondaryText,
-//   buildSlotText,
-//   selectConfirmOperationLabware,
-// } from "./index";
-// import { Input } from "../../components/forms/Input";
-// import Modal, { ModalBody, ModalFooter } from "../../components/Modal";
-// import Heading from "../../components/Heading";
-// import LayoutPlanner from "../../components/LayoutPlanner";
-// import BlueButton from "../../components/buttons/BlueButton";
-// import WhiteButton from "../../components/buttons/WhiteButton";
-// import PinkButton from "../../components/buttons/PinkButton";
-// import Labware from "../../components/labware/Labware";
-//
-// interface ConfirmTubesProps {
-//   layoutPlans: Array<LayoutPlan>;
-// }
-//
-// const ConfirmTubes: React.FC<ConfirmTubesProps> = ({ layoutPlans }) => {
-//   return (
-//     <div className="p-4 lg:w-2/3 lg:mx-auto rounded-lg bg-gray-100 space-y-4">
-//       <div>
-//         <p className="text-gray-800 text-sm leading-normal">
-//           For any tubes that were created but did not receive any sections, you
-//           can mark them as{" "}
-//           <span className="font-bold text-gray-900">unused</span> by clicking
-//           its cancel button.
-//         </p>
-//       </div>
-//       <div className="">
-//         <Table>
-//           <TableHead>
-//             <tr>
-//               <TableHeader />
-//               <TableHeader>Tube Barcode</TableHeader>
-//               <TableHeader>Section Number</TableHeader>
-//               <TableHeader />
-//             </tr>
-//           </TableHead>
-//           <TableBody>
-//             {layoutPlans.map((layoutPlan, i) => (
-//               <TubeRow key={i} initialLayoutPlan={layoutPlan} />
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </div>
-//     </div>
-//   );
-// };
-//
-// export default ConfirmTubes;
-//
-// interface TubeRowProps {
-//   initialLayoutPlan: LayoutPlan;
-// }
-//
-// const TubeRow: React.FC<TubeRowProps> = ({ initialLayoutPlan }) => {
-//   const model = useContext(SectioningPageContext)!;
-//   const [current, send, service] = useMachine(
-//     createSectioningConfirmMachine(
-//       model.context.comments,
-//       initialLayoutPlan.destinationLabware,
-//       initialLayoutPlan
-//     )
-//   );
-//
-//   const confirmOperationLabware = useSelector(
-//     service,
-//     selectConfirmOperationLabware
-//   );
-//
-//   const commitConfirmation = model.commitConfirmation;
-//
-//   useEffect(() => {
-//     if (confirmOperationLabware) {
-//       commitConfirmation(confirmOperationLabware);
-//     }
-//   }, [commitConfirmation, confirmOperationLabware]);
-//
-//   const { cancelled, layoutPlan, labware } = current.context;
-//   const { layoutMachine } = current.children;
-//
-//   const rowClassnames = classNames(
-//     {
-//       "opacity-50": cancelled,
-//     },
-//     "cursor-pointer hover:opacity-90 text-sm tracking-wide"
-//   );
-//
-//   const handleOnClick = useCallback(() => {
-//     send({ type: "TOGGLE_CANCEL" });
-//   }, [send]);
-//
-//   const handleOnChange = useCallback(
-//     (slotAddress: string, sectionNumber: number, sectionIndex: number) => {
-//       send({
-//         type: "UPDATE_SECTION_NUMBER",
-//         slotAddress,
-//         sectionNumber,
-//         sectionIndex,
-//       });
-//     },
-//     [send]
-//   );
-//
-//   return (
-//     <tr className={rowClassnames}>
-//       <TableCell>
-//         <div className="py-4 flex flex-col items-center justify-between space-y-8">
-//           <Labware
-//             labware={labware}
-//             onClick={() => {
-//               !cancelled && send({ type: "EDIT_LAYOUT" });
-//             }}
-//             slotText={(address) => buildSlotText(layoutPlan, address)}
-//             slotSecondaryText={(address) =>
-//               buildSlotSecondaryText(layoutPlan, address)
-//             }
-//             slotColor={(address) => buildSlotColor(layoutPlan, address)}
-//           />
-//
-//           <PinkButton
-//             disabled={current.matches("done")}
-//             onClick={() => !cancelled && send({ type: "EDIT_LAYOUT" })}
-//           >
-//             Edit Layout
-//           </PinkButton>
-//         </div>
-//       </TableCell>
-//
-//       <TableCell>
-//         <span className={`${cancelled ? "line-through" : ""}`}>
-//           {layoutPlan.destinationLabware.barcode}
-//         </span>
-//
-//         {layoutMachine && (
-//           <Modal show={true}>
-//             <ModalBody>
-//               <Heading level={3}>Set Layout</Heading>
-//               {layoutMachine && (
-//                 <LayoutPlanner actor={layoutMachine}>
-//                   <div className="my-2">
-//                     <p className="text-gray-900 text-sm leading-normal">
-//                       Click a slot to increase the number of sections in that
-//                       slot.
-//                     </p>
-//                     <p>
-//                       To reduce the number of sections in a slot, use
-//                       Ctrl-Click.
-//                     </p>
-//                   </div>
-//                 </LayoutPlanner>
-//               )}
-//             </ModalBody>
-//             <ModalFooter>
-//               <BlueButton
-//                 onClick={() => layoutMachine.send({ type: "DONE" })}
-//                 className="w-full text-base sm:ml-3 sm:w-auto sm:text-sm"
-//               >
-//                 Done
-//               </BlueButton>
-//               <WhiteButton
-//                 onClick={() => layoutMachine.send({ type: "CANCEL" })}
-//                 className="mt-3 w-full sm:mt-0 sm:ml-3"
-//               >
-//                 Cancel
-//               </WhiteButton>
-//             </ModalFooter>
-//           </Modal>
-//         )}
-//       </TableCell>
-//       <TableCell>
-//         {layoutPlan.plannedActions.get("A1")?.map((source, index) => (
-//           <Input
-//             key={source.address + String(index)}
-//             type="number"
-//             defaultValue={
-//               source.newSection === 0 ? "" : String(source.newSection)
-//             }
-//             min={1}
-//             disabled={cancelled}
-//             // So the row click handler doesn't get triggered
-//             onClick={(e) => e.stopPropagation()}
-//             onChange={(e) =>
-//               handleOnChange("A1", Number(e.target.value), index)
-//             }
-//           />
-//         ))}
-//       </TableCell>
-//       <TableCell onClick={handleOnClick}>
-//         <RemoveIcon className="h-4 w-4 text-red-500" />
-//       </TableCell>
-//     </tr>
-//   );
-// };
+import React, { useCallback, useContext, useEffect } from "react";
+import { useMachine, useSelector } from "@xstate/react";
+import classNames from "classnames";
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+} from "../../components/Table";
+import RemoveIcon from "../../components/icons/RemoveIcon";
+import { createSectioningConfirmMachine } from "../../lib/machines/sectioning/sectioningConfirm/sectioningConfirmMachine";
+import { LayoutPlan } from "../../lib/machines/layout/layoutContext";
+import { SectioningPageContext } from "../Sectioning";
+import {
+  buildSlotColor,
+  buildSlotSecondaryText,
+  buildSlotText,
+  selectConfirmOperationLabware,
+} from "./index";
+import { Input } from "../../components/forms/Input";
+import Modal, { ModalBody, ModalFooter } from "../../components/Modal";
+import Heading from "../../components/Heading";
+import LayoutPlanner from "../../components/LayoutPlanner";
+import BlueButton from "../../components/buttons/BlueButton";
+import WhiteButton from "../../components/buttons/WhiteButton";
+import PinkButton from "../../components/buttons/PinkButton";
+import Labware from "../../components/labware/Labware";
+
+interface ConfirmTubesProps {
+  layoutPlans: Array<LayoutPlan>;
+}
+
+const ConfirmTubes: React.FC<ConfirmTubesProps> = ({ layoutPlans }) => {
+  return (
+    <div className="p-4 lg:w-2/3 lg:mx-auto rounded-lg bg-gray-100 space-y-4">
+      <div>
+        <p className="text-gray-800 text-sm leading-normal">
+          For any tubes that were created but did not receive any sections, you
+          can mark them as{" "}
+          <span className="font-bold text-gray-900">unused</span> by clicking
+          its cancel button.
+        </p>
+      </div>
+      <div className="">
+        <Table>
+          <TableHead>
+            <tr>
+              <TableHeader />
+              <TableHeader>Tube Barcode</TableHeader>
+              <TableHeader>Section Number</TableHeader>
+              <TableHeader />
+            </tr>
+          </TableHead>
+          <TableBody>
+            {layoutPlans.map((layoutPlan, i) => (
+              <TubeRow key={i} initialLayoutPlan={layoutPlan} />
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmTubes;
+
+interface TubeRowProps {
+  initialLayoutPlan: LayoutPlan;
+}
+
+const TubeRow: React.FC<TubeRowProps> = ({ initialLayoutPlan }) => {
+  const model = useContext(SectioningPageContext)!;
+  const [current, send, service] = useMachine(
+    createSectioningConfirmMachine(
+      model.context.comments,
+      initialLayoutPlan.destinationLabware,
+      initialLayoutPlan
+    )
+  );
+
+  const confirmOperationLabware = useSelector(
+    service,
+    selectConfirmOperationLabware
+  );
+
+  const commitConfirmation = model.commitConfirmation;
+
+  useEffect(() => {
+    if (confirmOperationLabware) {
+      commitConfirmation(confirmOperationLabware);
+    }
+  }, [commitConfirmation, confirmOperationLabware]);
+
+  const { cancelled, layoutPlan, labware } = current.context;
+  const { layoutMachine } = current.children;
+
+  const rowClassnames = classNames(
+    {
+      "opacity-50": cancelled,
+    },
+    "cursor-pointer hover:opacity-90 text-sm tracking-wide"
+  );
+
+  const handleOnClick = useCallback(() => {
+    send({ type: "TOGGLE_CANCEL" });
+  }, [send]);
+
+  const handleOnChange = useCallback(
+    (slotAddress: string, sectionNumber: number, sectionIndex: number) => {
+      send({
+        type: "UPDATE_SECTION_NUMBER",
+        slotAddress,
+        sectionNumber,
+        sectionIndex,
+      });
+    },
+    [send]
+  );
+
+  return (
+    <tr className={rowClassnames}>
+      <TableCell>
+        <div className="py-4 flex flex-col items-center justify-between space-y-8">
+          <Labware
+            labware={labware}
+            onClick={() => {
+              !cancelled && send({ type: "EDIT_LAYOUT" });
+            }}
+            slotText={(address) => buildSlotText(layoutPlan, address)}
+            slotSecondaryText={(address) =>
+              buildSlotSecondaryText(layoutPlan, address)
+            }
+            slotColor={(address) => buildSlotColor(layoutPlan, address)}
+          />
+
+          <PinkButton
+            disabled={current.matches("done")}
+            onClick={() => !cancelled && send({ type: "EDIT_LAYOUT" })}
+          >
+            Edit Layout
+          </PinkButton>
+        </div>
+      </TableCell>
+
+      <TableCell>
+        <span className={`${cancelled ? "line-through" : ""}`}>
+          {layoutPlan.destinationLabware.barcode}
+        </span>
+
+        {layoutMachine && (
+          <Modal show={true}>
+            <ModalBody>
+              <Heading level={3}>Set Layout</Heading>
+              {layoutMachine && (
+                <LayoutPlanner actor={layoutMachine}>
+                  <div className="my-2">
+                    <p className="text-gray-900 text-sm leading-normal">
+                      Click a slot to increase the number of sections in that
+                      slot.
+                    </p>
+                    <p>
+                      To reduce the number of sections in a slot, use
+                      Ctrl-Click.
+                    </p>
+                  </div>
+                </LayoutPlanner>
+              )}
+            </ModalBody>
+            <ModalFooter>
+              <BlueButton
+                onClick={() => layoutMachine.send({ type: "DONE" })}
+                className="w-full text-base sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Done
+              </BlueButton>
+              <WhiteButton
+                onClick={() => layoutMachine.send({ type: "CANCEL" })}
+                className="mt-3 w-full sm:mt-0 sm:ml-3"
+              >
+                Cancel
+              </WhiteButton>
+            </ModalFooter>
+          </Modal>
+        )}
+      </TableCell>
+      <TableCell>
+        {layoutPlan.plannedActions.get("A1")?.map((source, index) => (
+          <Input
+            key={source.address + String(index)}
+            type="number"
+            defaultValue={
+              source.newSection === 0 ? "" : String(source.newSection)
+            }
+            min={1}
+            disabled={cancelled}
+            // So the row click handler doesn't get triggered
+            onClick={(e) => e.stopPropagation()}
+            onChange={(e) =>
+              handleOnChange("A1", Number(e.target.value), index)
+            }
+          />
+        ))}
+      </TableCell>
+      <TableCell onClick={handleOnClick}>
+        <RemoveIcon className="h-4 w-4 text-red-500" />
+      </TableCell>
+    </tr>
+  );
+};
