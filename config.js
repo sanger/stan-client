d78e29ba51fae9bcf395486e39fbb748c5980cc1
Chris Smith <cs24@sanger.ac.uk>
2021-10-28 17:27:47 +0100

    Refresh

--- config.js
+++
@@ -1,8 +0,0 @@
-window.STAN_CONFIG = {
-  env: "development",
-  headerColor: "bg-gradient-to-tr from-sdb to-sdb-400",
-  footerColor: "border-sdb-100 bg-sdb-400",
-  deploymentDate: "TBD",
-  supportEmail: "beaglebugs@sanger.ac.uk",
-  maxSearchRecords: 150,
-};
