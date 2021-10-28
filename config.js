fd1301e629bc821eeb2aca2de8cafc751ae78269
jbeast <chrissmith987@gmail.com>
2021-10-28 16:06:09 +0000

    Deploying to gh-pages from @ sanger/stan-client@ed0191cbde0fc486951e235a9af36a766a9a9e6b 🚀

---
+++ config.js
@@ -0,0 +1,8 @@
+window.STAN_CONFIG = {
+  env: "development",
+  headerColor: "bg-gradient-to-tr from-sdb to-sdb-400",
+  footerColor: "border-sdb-100 bg-sdb-400",
+  deploymentDate: "TBD",
+  supportEmail: "beaglebugs@sanger.ac.uk",
+  maxSearchRecords: 150,
+};
