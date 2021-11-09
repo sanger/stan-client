98727d5841695a6d292260d02471d98cc226e7c8
Seena Nair <55585488+seenanair@users.noreply.github.com>
2021-11-09 16:04:13 +0000

    x631 fix - Operation type change from "DV200" and "RIN"  to "DV200 analysis" and "RIN analysis".

--- src/components/analysisLabware/analysisLabware.tsx
+++ src/components/analysisLabware/analysisLabware.tsx
@@ -9,6 +9,7 @@ import DataTable from "../DataTable";
 import { useMachine } from "@xstate/react";
 import { analysisLabwareMachine } from "./analysisLabware.machine";
 import { measurementColumn } from "./measurementColumn";
+import { objectKeys } from "../../lib/helpers";
 
 type RecordAnalysisProps = {
   barcodes: string[];
@@ -20,8 +21,8 @@ type RecordAnalysisProps = {
   ) => void;
 };
 export enum OperationType {
-  RIN = "RIN",
-  DV200 = "DV200",
+  RIN = "RIN analysis",
+  DV200 = "DV200 analysis",
 }
 
 export default function AnalysisLabware({
@@ -166,8 +167,8 @@ export default function AnalysisLabware({
                   })
                 }
               >
-                {Object.values(OperationType).map((type) => (
-                  <option value={type} key={type}>
+                {objectKeys(OperationType).map((type) => (
+                  <option value={OperationType[type]} key={type}>
                     {type}
                   </option>
                 ))}
