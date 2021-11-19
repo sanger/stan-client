5f1a6ab57e8fcf2442bf1500e0c4cbe810c60237
Seena Nair <55585488+seenanair@users.noreply.github.com>
2021-11-19 20:07:48 +0000

    x671 - Visium cDNA Amplification
    - CDNAAmplification component
    - Update in VisiumQC to use Labware and Formik context in child components
    - Update in SlideProcessing to use Labware and Formik context

--- src/components/visiumQC/SlideProcessing.tsx
+++ src/components/visiumQC/SlideProcessing.tsx
@@ -9,23 +9,21 @@ import {
 import LabwareResult from "../labwareResult/LabwareResult";
 import { isSlotFilled } from "../../lib/helpers/slotHelper";
 import Panel from "../Panel";
-import { QCType } from "../../pages/VisiumQC";
+import { QCType, VisiumQCData } from "../../pages/VisiumQC";
 import { StanCoreContext } from "../../lib/sdk";
 import { useMachine } from "@xstate/react";
 import createFormMachine from "../../lib/machines/form/formMachine";
 import BlueButton from "../buttons/BlueButton";
 import { VisiumQCTypeProps } from "./VisiumQCType";
+import { useLabwareContext } from "../labwareScanner/LabwareScanner";
+import { useFormikContext } from "formik";
 
-const SlideProcessing = ({
-  workNumber,
-  labware,
-  removeLabware,
-  comments,
-  onSave,
-  onError,
-}: VisiumQCTypeProps) => {
+const SlideProcessing = ({ comments, onSave, onError }: VisiumQCTypeProps) => {
   const [labwareResult, setLabwareResult] = useState<CoreLabwareResult>();
   const stanCore = useContext(StanCoreContext);
+
+  const { labwares, removeLabware } = useLabwareContext();
+  const { values } = useFormikContext<VisiumQCData>();
 
   const [current, send] = useMachine(
     createFormMachine<ResultRequest, RecordVisiumQcMutation>().withConfig({
@@ -45,8 +43,10 @@ const SlideProcessing = ({
    * When labwares changes ,the labwareResults has to be updated accordingly
    */
   useEffect(() => {
-    setLabwareResult(labware ? buildLabwareResult(labware) : undefined);
-  }, [labware, setLabwareResult]);
+    setLabwareResult(
+      labwares.length > 0 ? buildLabwareResult(labwares[0]) : undefined
+    );
+  }, [labwares, setLabwareResult]);
 
   /***
    * Save(/Recording) operation completed.
@@ -75,12 +75,12 @@ const SlideProcessing = ({
 
   return (
     <>
-      {labwareResult && labware && (
-        <Panel key={labware.barcode}>
+      {labwareResult && labwares.length > 0 && (
+        <Panel key={labwares[0].barcode}>
           <LabwareResult
             initialLabwareResult={labwareResult}
-            labware={labware}
-            availableComments={comments}
+            labware={labwares[0]}
+            availableComments={comments ? comments : []}
             onRemoveClick={removeLabware}
             onChange={(labwareResult) => setLabwareResult(labwareResult)}
           />
@@ -89,13 +89,14 @@ const SlideProcessing = ({
 
       <div className={"mt-4 flex flex-row items-center justify-end"}>
         <BlueButton
-          disabled={!labware}
+          disabled={labwares.length <= 0}
           onClick={() => {
+            alert("Worknumber:" + values.workNumber);
             labwareResult &&
               send({
                 type: "SUBMIT_FORM",
                 values: {
-                  workNumber,
+                  workNumber: values.workNumber,
                   labwareResults: [labwareResult],
                   operationType: QCType.SLIDE_PROCESSING,
                 },
