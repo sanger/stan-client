bd179104658cd8bbb407cdc606128e29f9ccde55
Chris Smith <cs24@sanger.ac.uk>
2021-11-26 16:02:07 +0000

    Fixes RT#737516 (#92)
    * Fixes RT#737516
    
    In Chrome, inputs don't regain focus when they go from enabled, to disabled, then back to
    enabled (unlike Firefox). The code compensates for that by
    explicitly focusing an input after a barcode scan. However, the reference to
    the input wasn't being set up correctly. This tiny commit fixes that.
    
    * Don't focus labware input after a location scan

--- src/components/labwareScanner/LabwareScanner.tsx
+++ src/components/labwareScanner/LabwareScanner.tsx
@@ -96,7 +96,7 @@ export default function LabwareScanner({
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     const subscription = service.subscribe((observer) => {
-      if (observer.matches("idle")) {
+      if (observer.matches("idle") && !observer.context.locationScan) {
         inputRef.current?.focus();
       }
     });
@@ -202,6 +202,7 @@ export default function LabwareScanner({
             disabled={!current.matches("idle")}
             onChange={handleOnScanInputChange}
             onScan={handleOnScan}
+            ref={inputRef}
           />
         </div>
       </div>
