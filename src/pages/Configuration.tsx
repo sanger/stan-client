a7570483851a1e6cd46e73744d60a3a8b907ad19
Seena Nair <55585488+seenanair@users.noreply.github.com>
2022-06-17 12:08:54 +0100

    Merging devel to the branch

--- src/pages/Configuration.tsx
+++ src/pages/Configuration.tsx
@@ -31,7 +31,7 @@ export default function Configuration({ configuration }: ConfigurationParams) {
     "Release Destinations",
     "Release Recipients",
     "Species",
-    "Solution Samples",
+    "Solutions",
     "Users",
     "Work Types",
   ];
@@ -348,32 +348,31 @@ export default function Configuration({ configuration }: ConfigurationParams) {
           }}
         />
       </div>,
+      /**Solutions**/
       <div data-testid="config">
-        <Heading level={2}>Solution Sample</Heading>
+        <Heading level={2}>Solutions</Heading>
         <p className="mt-3 mb-6 text-lg">
-          Solution Samples are available on the{" "}
+          Solutions are available on the{" "}
           <StyledLink to={"/admin/tissue_registration"}>
-            Tissue Sample Registration
+            Original Sample Registration
           </StyledLink>{" "}
           page.
         </p>
         <EntityManager
-          initialEntities={configuration.solutionSamples}
+          initialEntities={configuration.solutions}
           displayKeyColumnName={"name"}
           valueColumnName={"enabled"}
           onChangeValue={(entity, value) => {
             const enabled = typeof value === "boolean" ? value : false;
             return stanCore
-              .SetSolutionSampleEnabled({
+              .SetSolutionEnabled({
                 name: entity.name,
                 enabled,
               })
-              .then((res) => res.setSolutionSampleEnabled);
+              .then((res) => res.setSolutionEnabled);
           }}
           onCreate={(name) =>
-            stanCore
-              .AddSolutionSample({ name })
-              .then((res) => res.addSolutionSample)
+            stanCore.AddSolution({ name }).then((res) => res.addSolution)
           }
           valueFieldComponentInfo={{
             type: "CHECKBOX",
