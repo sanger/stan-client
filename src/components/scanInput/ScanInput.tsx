8df459d7e9d4463be3ce5280b99452a7bcedaeed
Seena Nair <55585488+seenanair@users.noreply.github.com>
2022-07-25 14:58:28 +0100

    Tailwind configuration from @sanger/ui-styling package

--- src/components/scanInput/ScanInput.tsx
+++ src/components/scanInput/ScanInput.tsx
@@ -25,7 +25,7 @@ const ScanInput = React.forwardRef<HTMLInputElement, ScanInputProps>(
         "rounded-r-md": !inputProps?.disabled,
         "border-r-0 disabled:bg-gray-100": inputProps?.disabled,
       },
-      "flex-grow-0 focus:ring-sdb-100 focus:border-sdb-100 block w-full border-gray-300 rounded-none transition duration-150 ease-in-out"
+      "flex-grow-0 focus:ring-sdb-100 focus:border-sdb-100 h-10 block w-full border-gray-300 rounded-none transition duration-150 ease-in-out"
     );
 
     const onKeyDownHandler = React.useCallback(
