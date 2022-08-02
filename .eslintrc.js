520ec87ada085c93c24dba18ea22b3a26caadedf
Ben Topping <ben.topping@outlook.com>
2022-08-02 14:22:01 +0100

    Intial work on adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "prettier"
+  ],
+  "plugins": [
+    "prettier",
+  ],
+  "rules": {
+    "prettier/prettier": ["error"]
+  },
+  "parser": "@typescript-eslint/parser"
+}
