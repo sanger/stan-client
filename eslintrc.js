3b6759433d1a4626c38563fd4a5ba8aae0fac740
Ben Topping <ben.topping@outlook.com>
2022-08-04 10:19:27 +0100

    Turnedeslint  rules back on

--- eslintrc.js
+++ eslintrc.js
@@ -2,13 +2,7 @@ module.exports = {
   extends: ['eslint:recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error'],
-    /* 
-        Turned off no-undef and no-unused-vars rules as they were flagging a lot of code
-        Possibly caused by lack of react plugin or wrong parser
-    */
-    'no-undef': 0,
-    'no-unused-vars': 0
+    'prettier/prettier': ['error']
   },
   parser: '@typescript-eslint/parser'
 };
