8c54644fb1d434f668fc7541d63bb44c61a01acc
Seena Nair <55585488+seenanair@users.noreply.github.com>
2022-10-26 12:08:52 +0100

    Unit test cases update

--- tests/unit/components/slotMapper/reagentTransferSlotMapper.spec.tsx
+++ tests/unit/components/slotMapper/reagentTransferSlotMapper.spec.tsx
@@ -8,31 +8,28 @@ enableMapSet();
 afterEach(() => {
   cleanup();
 });
+const srcLabware = createLabware('123456789123456789012345');
+const destLabware = createLabware('STAN-5111');
+
 describe('ReagentTransferSlotMapper.tsx', () => {
   it('displays the parent div elements for source and destination', () => {
-    const dom = render(<ReagentTransferSlotMapper initialSourceLabware={undefined} initialDestLabware={undefined} />);
+    const reagentSlotMapper = render(
+      <ReagentTransferSlotMapper initialSourceLabware={undefined} initialDestLabware={undefined} />
+    );
 
     //Displays div element for source labware
-    const sourceLabwareElem = getById(dom.container, 'sourceLabwares');
+    const sourceLabwareElem = getById(reagentSlotMapper.container, 'sourceLabwares');
     expect(sourceLabwareElem).not.toBeNull();
 
     //Displays div element for destination labware
-    const destLabwareElem = getById(dom.container, 'destLabwares');
+    const destLabwareElem = getById(reagentSlotMapper.container, 'destLabwares');
     expect(destLabwareElem).not.toBeNull();
   });
 
-  it('displays source', () => {
-    const srcLabware = createLabware('123456789123456789012345');
-
-    render(<ReagentTransferSlotMapper initialSourceLabware={srcLabware} initialDestLabware={undefined} />);
-    //Displays source labware element
+  it('displays source and destination', () => {
+    render(<ReagentTransferSlotMapper initialSourceLabware={srcLabware} initialDestLabware={destLabware} />);
+    //Displays source and destination labware element
     expect(screen.getByText('123456789123456789012345')).toBeInTheDocument();
-  });
-  it('displays destination', () => {
-    const destLabware = createLabware('STAN-5111');
-    render(<ReagentTransferSlotMapper initialSourceLabware={undefined} initialDestLabware={destLabware} />);
-
-    //Displays source labware element
     expect(screen.getByText('STAN-5111')).toBeInTheDocument();
   });
 });
