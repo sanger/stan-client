c7cc77a6636daeeb44eb621a59d0a4748f4cca8a
Seena Nair <55585488+seenanair@users.noreply.github.com>
2023-02-14 12:35:14 +0000

    Test case updates

--- cypress/e2e/pages/unrelease.cy.ts
+++ cypress/e2e/pages/unrelease.cy.ts
@@ -64,15 +64,9 @@ describe('Unrelease Page', () => {
         cy.wait(1000);
         cy.get('#labwareScanInput').should('not.be.disabled').type('STAN-3112{enter}');
         cy.findByRole('button', { name: /Submit/i }).click();
-        cy.on('uncaught:exception', () => {
-          return false;
-        });
       });
 
       it('shows the server errors', () => {
-        cy.on('uncaught:exception', () => {
-          return false;
-        });
         cy.findByText('This thing went wrong').should('be.visible');
         cy.findByText('This other thing went wrong').should('be.visible');
       });
@@ -82,6 +76,7 @@ describe('Unrelease Page', () => {
       before(() => {
         cy.visit('admin/unrelease');
         selectSGPNumber('SGP1008');
+        cy.wait(1000);
         cy.get('#labwareScanInput').should('not.be.disabled').type('STAN-3111{enter}');
         cy.findByRole('button', { name: /Submit/i }).click();
       });
