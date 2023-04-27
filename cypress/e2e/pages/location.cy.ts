bd5f86cd84f9826b5a9fd0a12129b16430254327
Seena Nair <55585488+seenanair@users.noreply.github.com>
2023-04-27 11:08:08 +0100

    Cypress test case failure test on CI run

--- cypress/e2e/pages/location.cy.ts
+++ cypress/e2e/pages/location.cy.ts
@@ -157,7 +157,6 @@ describe('Location', () => {
         cy.reload();
       });
       it('should display the updated list of awaiting labwares', () => {
-        cy.reload();
         cy.findByText('STAN-2111').should('exist');
         cy.findByText('STAN-4111').should('exist');
         cy.findByText('STAN-3111').should('not.exist');
