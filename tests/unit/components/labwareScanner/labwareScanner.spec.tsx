cd91dfb1b1a8524cc068e976813841f84066419d
Seena Nair <55585488+seenanair@users.noreply.github.com>
2023-07-06 12:00:57 +0100

    Unit test case for labware scanner

--- tests/unit/components/labwareScanner/labwareScanner.spec.tsx
+++ tests/unit/components/labwareScanner/labwareScanner.spec.tsx
@@ -1,81 +1,122 @@
 import { act, cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
-import ScanInput from '../../../../src/components/scanInput/ScanInput';
-import { Formik } from 'formik';
 import LabwareScanner, { useLabwareContext } from '../../../../src/components/labwareScanner/LabwareScanner';
 import { getById } from '../../generic/utilities';
 import React from 'react';
+import { plateFactory } from '../../../../src/lib/factories/labwareFactory';
+import { LabwareFieldsFragment } from '../../../../src/types/sdk';
 
 afterEach(() => {
   cleanup();
 });
+
 describe('LabwareScannaer', () => {
-  describe('Labware Scanner with location', () => {
-    describe('On Mount', () => {
-      it('displays location and labware scan inputs', async () => {
-        let { container } = render(<LabwareScanner enableLocationScanner={true}>{}</LabwareScanner>);
+  describe('On Mount', () => {
+    it('displays location and labware scan inputs', async () => {
+      let { container } = render(<LabwareScanner enableLocationScanner={true}>{}</LabwareScanner>);
 
-        expect(screen.getByText('Location:')).toBeVisible();
-        expect(screen.getByText('Labware:')).toBeVisible();
-        expect(getById(container, 'locationScanInput')).toBeVisible();
-        expect(getById(container, 'labwareScanInput')).toBeVisible();
-        expect(screen.getAllByTestId('input')).toHaveLength(2);
-      });
-      it('does not displays location scan inputs', async () => {
-        let { container } = render(<LabwareScanner enableLocationScanner={false}>{}</LabwareScanner>);
-
-        //expect(screen.getByText('Labware:')).not.toBeInTheDocument();
-        //expect(screen.getByText('Location:')).not.toBeInTheDocument();
-        expect(getById(container, 'locationScanInput')).not.toBeInTheDocument();
-        expect(getById(container, 'labwareScanInput')).toBeVisible();
-        expect(screen.getAllByTestId('input')).toHaveLength(1);
-      });
-      it('displays entered value in location scanner', async () => {
-        act(() => {
-          render(<LabwareScanner enableLocationScanner={true}>{}</LabwareScanner>);
-        });
-        expect(screen.getAllByTestId('input')).toHaveLength(2);
-        const input = screen.getAllByTestId('input')[0] as HTMLInputElement;
-        await waitFor(() => {
-          fireEvent.change(input, { target: { value: '123' } });
-        });
-        expect(input).toHaveValue('123');
-      });
+      expect(screen.getByText('Location:')).toBeVisible();
+      expect(screen.getByText('Labware:')).toBeVisible();
+      expect(getById(container, 'locationScanInput')).toBeVisible();
+      expect(getById(container, 'labwareScanInput')).toBeVisible();
+      expect(screen.getAllByTestId('input')).toHaveLength(2);
+    });
+    it('does not displays location scan inputs', async () => {
+      let { container } = render(<LabwareScanner enableLocationScanner={false}>{}</LabwareScanner>);
+      expect(screen).not.toContain('Labware:');
+      expect(screen).not.toContain('Location:');
+      expect(getById(container, 'locationScanInput')).not.toBeInTheDocument();
+      expect(getById(container, 'labwareScanInput')).toBeVisible();
+      expect(screen.getAllByTestId('input')).toHaveLength(1);
     });
   });
-  /*describe('Formik Input component', () => {
-        describe('On Mount', () => {
-            it('displays input component with scan icon', async () => {
-                act(() => {
-                    render(
-                        <Formik {...FormikProps}>
-                            <ScanInput name={'testing'} label={'Label'} />
-                        </Formik>
-                    );
-                });
-                const input = (await screen.findByTestId('formInput')) as HTMLInputElement;
-                //Shows the formik input component
-                expect(input).toBeInTheDocument();
-                //Displays empty value
-                expect(input).toHaveValue('');
-                //Show the label
-                expect(screen.getByText('Label')).toBeVisible();
-            });
-        });
-        describe('on Entering value', () => {
-            it('displays entered value', async () => {
-                act(() => {
-                    render(
-                        <Formik {...FormikProps}>
-                            <ScanInput name={'testing'} label={'Label'} />
-                        </Formik>
-                    );
-                });
-                const input = (await screen.findByTestId('formInput')) as HTMLInputElement;
-                await waitFor(() => {
-                    fireEvent.change(input, { target: { value: '123' } });
-                });
-                expect(input).toHaveValue('123');
-            });
-        });
-    });*/
+  describe('on Entering value', () => {
+    it('displays entered value in location scanner', async () => {
+      act(() => {
+        render(<LabwareScanner enableLocationScanner={true}>{}</LabwareScanner>);
+      });
+      expect(screen.getAllByTestId('input')).toHaveLength(2);
+      const input = screen.getAllByTestId('input')[0] as HTMLInputElement;
+      await waitFor(() => {
+        fireEvent.change(input, { target: { value: '123' } });
+      });
+      expect(input).toHaveValue('123');
+    });
+    it('displays entered value in labware scanner', async () => {
+      act(() => {
+        render(<LabwareScanner enableLocationScanner={true}>{}</LabwareScanner>);
+      });
+      expect(screen.getAllByTestId('input')).toHaveLength(2);
+      const input = screen.getAllByTestId('input')[1] as HTMLInputElement;
+      await waitFor(() => {
+        fireEvent.change(input, { target: { value: '123' } });
+      });
+      expect(input).toHaveValue('123');
+    });
+  });
+
+  describe('on Providing children', () => {
+    const List = () => {
+      const { labwares, removeLabware } = useLabwareContext();
+      return (
+        <ul data-testid={'list'}>
+          {labwares.map((lw, indx) => (
+            <li data-testid={'list-item'} key={indx}>
+              {lw.barcode}
+              <button
+                data-testid={'removeButton'}
+                onClick={() => removeLabware(lw.barcode)}
+                className="text-red-500 font-bold underline"
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      );
+    };
+    it('displays child components', async () => {
+      act(() => {
+        render(
+          <LabwareScanner enableLocationScanner={true}>
+            <List />
+          </LabwareScanner>
+        );
+      });
+      expect(screen.getByTestId('list')).toBeVisible();
+    });
+    it('displays contextual information on entering value in labware scanner', async () => {
+      const inputLabware = plateFactory.build({ barcode: 'STAN-3111' });
+      const labware: LabwareFieldsFragment = { ...inputLabware, barcode: inputLabware.barcode ?? '' };
+
+      act(() => {
+        render(
+          <LabwareScanner enableLocationScanner={true} initialLabwares={[labware]}>
+            <List />
+          </LabwareScanner>
+        );
+      });
+      expect(screen.getAllByTestId('input')).toHaveLength(2);
+      const listItem = screen.getByTestId('list-item');
+      expect(listItem).toBeVisible();
+      expect(listItem).toHaveTextContent('STAN-3111');
+      expect(screen.getByTestId('removeButton')).toBeVisible();
+    });
+    it('removes labware on remove button click', async () => {
+      const inputLabware = plateFactory.build({ barcode: 'STAN-3111' });
+      const labware: LabwareFieldsFragment = { ...inputLabware, barcode: inputLabware.barcode ?? '' };
+
+      act(() => {
+        render(
+          <LabwareScanner enableLocationScanner={true} initialLabwares={[labware]}>
+            <List />
+          </LabwareScanner>
+        );
+      });
+      await waitFor(() => {
+        fireEvent.click(screen.getByTestId('removeButton'));
+      });
+      expect(screen.queryByTestId('list-item')).not.toBeInTheDocument();
+      expect(screen).not.toContain('STAN-3111');
+    });
+  });
 });
