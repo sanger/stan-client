9e947d8168b6ccab6d89cdd01f6766712148c44e
sabrine33 <sa33@sanger.ac.uk>
2023-08-29 13:24:21 +0100

    fixing the File uploader component when the response does not contain JSON

--- src/components/upload/useUpload.ts
+++ src/components/upload/useUpload.ts
@@ -19,7 +19,7 @@ export function useUpload(url: string, errorField?: string) {
 
   /**External request for upload**/
   const requestUpload = React.useCallback(
-    (file: File) => {
+    (file: File, setUploadInProgress) => {
       async function postUpload(url: string, file: File) {
         const formData = new FormData();
         formData.append('file', file);
@@ -35,15 +35,22 @@ export function useUpload(url: string, errorField?: string) {
       if (!file) return;
       postUpload(url, file)
         .then((response) => {
+          setUploadInProgress(false);
           const success = response.ok;
-          setUploadSuccess(success);
-          response.json().then((response) => {
-            if (!success) {
-              setError(new Error(errorField ? response[errorField] : response.message));
-            } else {
-              setUploadResponse(response);
-            }
-          });
+          if (success) setUploadSuccess(true);
+          response
+            .json()
+            .then((response) => {
+              if (!success) {
+                setError(new Error(errorField ? response[errorField] : response.message));
+              } else {
+                setUploadResponse(response);
+              }
+            })
+            .catch((error) => {
+              // setting it so the notifyUploadOutcome still been called from the FileUpload component when the response does not contain a JSON
+              setUploadResponse(error);
+            });
         })
         .catch((error) => {
           setError(new Error(error));
