a7a8ad430f1d0bc055b536b2eae3f341e104830b
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-01-15 15:27:44 +0000

    Update labwareDetails.cy.ts

--- cypress/e2e/pages/labwareDetails.cy.ts
+++ cypress/e2e/pages/labwareDetails.cy.ts
@@ -37,11 +37,15 @@ describe('Labware Info Page', () => {
     });
   });
 
-  context('when I visit as a guest', () => {
+  context('when I check a flagged labware details', () => {
     before(() => {
-      cy.visitAsGuest('/labware/STAN-0001F');
+      cy.visitAsGuest('/labware/STAN-100');
     });
 
+    it('display the related flags table', () => {
+      cy.findByText('Related Flags').should('be.visible');
+      cy.findAllByTestId('flag-icon').should('have.length.at.least', 1);
+    });
     it('does not show the label printer', () => {
       cy.findByText('Print Labels').should('not.exist');
     });
