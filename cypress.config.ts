72369d1c25e029fc954fb8d40ed13de234e9c203
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-01-24 16:11:22 +0000

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   video: false,
   defaultCommandTimeout: 15000,
   retries: 2,
+  numTestsKeptInMemory: 20,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
