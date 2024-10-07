591c00e75beb8c2c2e5067bed8020f069c2efd9a
sabrine33 <sa33@sanger.ac.uk>
2024-10-07 16:05:21 +0100

    I want to store labware after unreleasing them in STAN

--- src/pages/StanForm.tsx
+++ src/pages/StanForm.tsx
@@ -12,6 +12,9 @@ import PinkButton from '../components/buttons/PinkButton';
 import createFormMachine from '../lib/machines/form/formMachine';
 import OperationCompleteModal from '../components/modal/OperationCompleteModal';
 import { fromPromise } from 'xstate';
+import WhiteButton from '../components/buttons/WhiteButton';
+import { createSessionStorageForLabwareAwaiting } from '../types/stan';
+import { useNavigate } from 'react-router-dom';
 
 type StanFormParams<V, R> = {
   /**
@@ -44,6 +47,11 @@ type StanFormParams<V, R> = {
    * Children of StanForm should be the fields of the form
    */
   children: (props: FormikProps<V>) => React.ReactNode;
+
+  /**
+   * Used in case we need to display a "Store" redirect button once the operation is successful
+   */
+  displayStoreOption?: boolean;
 };
 
 /**
@@ -55,7 +63,8 @@ export default function StanForm<V extends FormikValues, R>({
   validationSchema,
   initialValues,
   summary,
-  children
+  children,
+  displayStoreOption
 }: StanFormParams<V, R>) {
   const formMachine = React.useMemo(() => {
     return createFormMachine<FormikValues, R>().provide({
@@ -68,6 +77,7 @@ export default function StanForm<V extends FormikValues, R>({
     });
   }, [onSubmit]);
   const [current, send] = useMachine(formMachine);
+  const navigate = useNavigate();
   const submitForm = async (values: V) => send({ type: 'SUBMIT_FORM', values });
   const serverError = current.context.serverError;
   const formLocked = !current.matches('fillingOutForm');
@@ -79,43 +89,70 @@ export default function StanForm<V extends FormikValues, R>({
       </AppShell.Header>
       <AppShell.Main>
         <div className="max-w-screen-xl mx-auto">
-          <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={submitForm}>
-            {(formikProps) => (
-              <Form>
-                <GrayBox>
-                  <motion.div
-                    variants={variants.fadeInParent}
-                    initial={'hidden'}
-                    animate={'visible'}
-                    exit={'hidden'}
-                    className="md:w-2/3 space-y-10"
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={submitForm}
+            validateOnMount={true}
+          >
+            {(formikProps) => {
+              return (
+                <Form>
+                  <GrayBox>
+                    <motion.div
+                      variants={variants.fadeInParent}
+                      initial={'hidden'}
+                      animate={'visible'}
+                      exit={'hidden'}
+                      className="md:w-2/3 space-y-10"
+                    >
+                      {serverError && <Warning error={serverError} />}
+
+                      {children(formikProps)}
+                    </motion.div>
+
+                    <Sidebar>
+                      <Heading level={3} showBorder={false}>
+                        Summary
+                      </Heading>
+
+                      {summary(formikProps)}
+
+                      <PinkButton disabled={formLocked || !formikProps.isValid} type="submit" className="sm:w-full">
+                        Submit
+                      </PinkButton>
+                    </Sidebar>
+                  </GrayBox>
+
+                  <OperationCompleteModal
+                    show={current.matches('submitted')}
+                    message={'Operation Complete'}
+                    additionalButtons={
+                      displayStoreOption && formikProps.values.labware.length > 0 ? (
+                        <WhiteButton
+                          type="button"
+                          style={{ marginLeft: 'auto' }}
+                          className="w-full text-base md:ml-0 sm:ml-3 sm:w-auto sm:text:sm"
+                          onClick={() => {
+                            createSessionStorageForLabwareAwaiting(formikProps.values.labware);
+                            navigate('/store');
+                          }}
+                        >
+                          Store
+                        </WhiteButton>
+                      ) : (
+                        <></>
+                      )
+                    }
                   >
-                    {serverError && <Warning error={serverError} />}
-
-                    {children(formikProps)}
-                  </motion.div>
-
-                  <Sidebar>
-                    <Heading level={3} showBorder={false}>
-                      Summary
-                    </Heading>
-
-                    {summary(formikProps)}
-
-                    <PinkButton disabled={formLocked} type="submit" className="sm:w-full">
-                      Submit
-                    </PinkButton>
-                  </Sidebar>
-                </GrayBox>
-
-                <OperationCompleteModal show={current.matches('submitted')} message={'Operation Complete'}>
-                  <p>
-                    If you wish to start the process again, click the "Reset Form" button. Otherwise you can return to
-                    the Home screen.
-                  </p>
-                </OperationCompleteModal>
-              </Form>
-            )}
+                    <p>
+                      If you wish to start the process again, click the "Reset Form" button. Otherwise you can return to
+                      the Home screen.
+                    </p>
+                  </OperationCompleteModal>
+                </Form>
+              );
+            }}
           </Formik>
         </div>
       </AppShell.Main>
--- src/pages/Unrelease.tsx
+++ src/pages/Unrelease.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import StanForm from './StanForm';
 import { stanCore } from '../lib/sdk';
-import {
-  LabwareFieldsFragment,
-  LabwareFlaggedFieldsFragment,
-  UnreleaseLabware,
-  UnreleaseMutation,
-  UnreleaseRequest
-} from '../types/sdk';
+import { LabwareFieldsFragment, LabwareFlaggedFieldsFragment, UnreleaseMutation } from '../types/sdk';
 import * as Yup from 'yup';
 import LabwareScanner from '../components/labwareScanner/LabwareScanner';
 import LabwareScanPanel from '../components/labwareScanPanel/LabwareScanPanel';
@@ -20,30 +14,31 @@ import MutedText from '../components/MutedText';
 import { hasBlock } from '../lib/helpers/labwareHelper';
 import { CellProps, Column } from 'react-table';
 import FormikInput from '../components/forms/Input';
-import { FieldArray } from 'formik';
 import { identity } from 'lodash';
 import WorkNumberSelect from '../components/WorkNumberSelect';
 import { parseQueryString } from '../lib/helpers';
 import { useLocation } from 'react-router-dom';
 import LoadingSpinner from '../components/icons/LoadingSpinner';
+import { FieldArray } from 'formik';
+
+type UnreleaseLabwareForm = LabwareFlaggedFieldsFragment & { blockHighestSection?: number | null };
+
+type UnreleaseRequestForm = {
+  workNumber: string;
+  labware: Array<UnreleaseLabwareForm>;
+};
 
 const validationSchema = Yup.object().shape({
+  workNumber: Yup.string().required('SGP Number is a required field'),
   labware: Yup.array()
     .label('Labware')
     .min(1, 'Please scan in at least 1 labware')
     .of(
       Yup.object().shape({
         barcode: Yup.string().required(),
-        highestSection: Yup.number().min(0, 'Section number must be greater than or equal to 0').notRequired(),
-        workNumber: Yup.string().required('SGP Number is a required field')
+        blockHighestSection: Yup.number().min(0, 'Section number must be greater than or equal to 0').notRequired()
       })
     )
-});
-
-const toUnreleaseLabware = (labware: LabwareFlaggedFieldsFragment, workNumber: string): UnreleaseLabware => ({
-  barcode: labware.barcode,
-  highestSection: hasBlock(labware) ? labware.slots[0].blockHighestSection : undefined,
-  workNumber
 });
 
 const fetchInitialLabware = async (initialBarcodes: string[]) => {
@@ -53,9 +48,14 @@ const fetchInitialLabware = async (initialBarcodes: string[]) => {
   );
 };
 
-export default function Unrelease() {
-  const workNumberRef = React.useRef('');
+const blockHighestSection = (labware: LabwareFlaggedFieldsFragment) => {
+  if (hasBlock(labware)) {
+    return labware.slots[0].blockHighestSection;
+  }
+  return undefined;
+};
 
+export default function Unrelease() {
   const location = useLocation();
 
   const initialBarcodes: string[] = React.useMemo(() => {
@@ -65,81 +65,85 @@ export default function Unrelease() {
       ? queryString['barcode'].map((barcode: string) => decodeURIComponent(barcode))
       : [decodeURIComponent(queryString['barcode'])];
   }, [location.search]);
-
   const [isFetching, setIsFetching] = React.useState(true);
-  const initialLabware = React.useRef<LabwareFlaggedFieldsFragment[]>([]);
-  fetchInitialLabware(initialBarcodes)
-    .then((labwares) => {
-      initialLabware.current = labwares;
-      setIsFetching(false);
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-    .finally(() => {
-      setIsFetching(false);
-    });
+  const initialLabware = React.useRef<UnreleaseLabwareForm[]>([]);
 
   if (isFetching) {
+    fetchInitialLabware(initialBarcodes)
+      .then((labwares) => {
+        initialLabware.current = labwares.map((lw) => ({ ...lw, blockHighestSection: blockHighestSection(lw) }));
+        setIsFetching(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsFetching(false);
+      });
+
     return <LoadingSpinner />;
   }
   return (
-    <StanForm<UnreleaseRequest, UnreleaseMutation>
+    <StanForm<UnreleaseRequestForm, UnreleaseMutation>
       title={'Unrelease'}
-      onSubmit={(request) => stanCore.Unrelease({ request })}
-      validationSchema={validationSchema}
-      initialValues={{
-        labware: initialLabware.current.map((lw) => toUnreleaseLabware(lw, workNumberRef.current))
+      onSubmit={(request) => {
+        return stanCore.Unrelease({
+          request: {
+            labware: request.labware.map((lw) => ({
+              barcode: lw.barcode,
+              highestSection: lw.blockHighestSection,
+              workNumber: request.workNumber
+            }))
+          }
+        });
       }}
+      validationSchema={validationSchema}
+      initialValues={{ workNumber: '', labware: initialLabware.current }}
       summary={(props) => (
         <p>
           <span className="font-bold">{props.values.labware.length}</span> labware scanned for Unrelease.
         </p>
       )}
+      displayStoreOption={true}
     >
-      {(formikProps) => {
-        return (
-          <motion.div variants={variants.fadeInWithLift}>
-            <motion.div variants={variants.fadeInWithLift} className={'mb-8'}>
-              <Heading level={3}>SGP Number</Heading>
-              <p className="mt-2">Please select an SGP number to associate with all labware</p>
-              <motion.div variants={variants.fadeInWithLift} className="mt-4 md:w-1/2">
-                <WorkNumberSelect
-                  onWorkNumberChange={(workNumber) => {
-                    workNumberRef.current = workNumber;
-                    formikProps.values.labware.forEach((lw) => (lw.workNumber = workNumber));
-                  }}
-                />
-              </motion.div>
-            </motion.div>
-
-            <motion.div variants={variants.fadeInWithLift} className="space-y-4">
-              <Heading level={3}>Labware</Heading>
-              <MutedText>Please scan in the labware you wish to unrelease.</MutedText>
-              <FieldArray name={'labware'}>
-                {(helpers) => {
-                  return (
-                    <LabwareScanner
-                      initialLabwares={initialLabware.current}
-                      onAdd={(lw) => helpers.push(toUnreleaseLabware(lw, workNumberRef.current))}
-                      onRemove={(labware, index) => helpers.remove(index)}
-                    >
-                      <LabwareScanPanel
-                        columns={[
-                          columns.barcode(),
-                          columns.externalName(),
-                          sectionNumberInputIfBlock(formikProps.values)
-                        ]}
-                      />
-                    </LabwareScanner>
-                  );
+      {({ setFieldValue }) => (
+        <motion.div variants={variants.fadeInWithLift}>
+          <motion.div variants={variants.fadeInWithLift} className={'mb-8'}>
+            <Heading level={3}>SGP Number</Heading>
+            <p className="mt-2">Please select an SGP number to associate with all labware</p>
+            <motion.div variants={variants.fadeInWithLift} className="mt-4 md:w-1/2">
+              <WorkNumberSelect
+                onWorkNumberChange={async (workNumber) => {
+                  await setFieldValue('workNumber', workNumber);
                 }}
-              </FieldArray>
-              <FormikErrorMessage name={'barcodes'} />
+              />
             </motion.div>
           </motion.div>
-        );
-      }}
+
+          <motion.div variants={variants.fadeInWithLift} className="space-y-4">
+            <Heading level={3}>Labware</Heading>
+            <MutedText>Please scan in the labware you wish to unrelease.</MutedText>
+            <FieldArray name={'labware'}>
+              {(helpers) => (
+                <LabwareScanner
+                  initialLabwares={initialLabware.current}
+                  onAdd={(lw) => helpers.push({ ...lw, blockHighestSection: blockHighestSection(lw) })}
+                  onRemove={(labware, index) => helpers.remove(index)}
+                >
+                  <LabwareScanPanel
+                    columns={[
+                      columns.barcode(),
+                      columns.externalName(),
+                      sectionNumberInputIfBlock(helpers.form.values.labware)
+                    ]}
+                  />
+                </LabwareScanner>
+              )}
+            </FieldArray>
+            <FormikErrorMessage name={'barcodes'} />
+          </motion.div>
+        </motion.div>
+      )}
     </StanForm>
   );
 }
@@ -147,16 +151,16 @@ export default function Unrelease() {
 /**
  * Column that will display a number input if the labware contains a block
  */
-function sectionNumberInputIfBlock(unreleaseRequest: UnreleaseRequest): Column<LabwareFieldsFragment> {
+function sectionNumberInputIfBlock(labware: UnreleaseLabwareForm[]): Column<LabwareFieldsFragment> {
   return {
     Header: 'Highest Section for Block',
     accessor: identity,
     Cell: (props: CellProps<LabwareFieldsFragment>) => {
-      const labwareIndex = unreleaseRequest.labware.findIndex((lw) => lw.barcode === props.value.barcode);
-
-      return hasBlock(props.value) ? (
-        <FormikInput label={''} name={`labware.${labwareIndex}.highestSection`} type={'number'} />
-      ) : null;
+      const labwareIndex = labware.findIndex((lw) => lw.barcode === props.value.barcode);
+      if (labwareIndex > -1 && labware[labwareIndex].blockHighestSection !== undefined) {
+        return <FormikInput label={''} name={`labware.${labwareIndex}.blockHighestSection`} type={'number'} />;
+      }
+      return '-';
     }
   };
 }
