239b8c7fe21221943a488ac91a9956bc565ccef4
sabrine33 <sa33@sanger.ac.uk>
2025-08-13 10:56:15 +0100

    fix spec tests

--- tests/unit/components/release/releaseOptions.spec.tsx
+++ tests/unit/components/release/releaseOptions.spec.tsx
@@ -1,7 +1,9 @@
-import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { createMemoryRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router';
 import ReleaseOptions from '../../../../src/components/release/ReleaseOptions';
+import React from 'react';
 
 afterEach(() => {
   cleanup();
@@ -9,43 +11,47 @@ afterEach(() => {
 const navigateFunction = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...(jest.requireActual('react-router-dom') as any),
-  useLoaderData: () => [
-    { displayName: 'Option 1', queryParamName: 'option_1' },
-    { displayName: 'Option 2', queryParamName: 'option_2' },
-    { displayName: 'Option 3', queryParamName: 'option_3' }
-  ],
   useNavigate: () => navigateFunction
 }));
+
 describe('ReleaseOptions', () => {
   it('renders page with release options', () => {
-    const router = createMemoryRouter([{ path: '/releaseOptions', element: <ReleaseOptions /> }], {
-      initialEntries: ['/releaseOptions?id=123']
+    act(() => {
+      const router = createMemoryRouter([{ path: '/releaseOptions', element: <ReleaseOptions /> }], {
+        initialEntries: ['/releaseOptions?id=123']
+      });
+      render(<RouterProvider router={router} />);
     });
-    render(<RouterProvider router={router} />);
-    expect(screen.getByText('Release File Options')).toBeInTheDocument();
-    expect(screen.getAllByRole('checkbox').length).toBe(3);
-    expect(screen.getByText('Option 1')).toBeInTheDocument();
-    expect(screen.getByText('Option 2')).toBeInTheDocument();
-    expect(screen.getByText('Option 3')).toBeInTheDocument();
-    ['Option 1', 'Option 2', 'Option 3'].forEach((option, indx) => {
-      expect(screen.getByText(option)).toBeInTheDocument();
-      const optionCheckBox = screen.getAllByRole('checkbox')[indx];
-      expect(optionCheckBox).not.toBeChecked();
+    waitFor(() => {
+      expect(screen.getByText('Release File Options')).toBeInTheDocument();
+      expect(screen.getAllByRole('checkbox').length).toBe(3);
+      expect(screen.getByText('Option 1')).toBeInTheDocument();
+      expect(screen.getByText('Option 2')).toBeInTheDocument();
+      expect(screen.getByText('Option 3')).toBeInTheDocument();
+      ['Option 1', 'Option 2', 'Option 3'].forEach((option, indx) => {
+        expect(screen.getByText(option)).toBeInTheDocument();
+        const optionCheckBox = screen.getAllByRole('checkbox')[indx];
+        expect(optionCheckBox).not.toBeChecked();
+      });
     });
   });
   it('On initial loading release options are selected based on query params', () => {
-    const router = createMemoryRouter([{ path: '/releaseOptions', element: <ReleaseOptions /> }], {
-      initialEntries: ['/releaseOptions?id=123&groups=option_1,option_2&type=xlsx']
+    act(() => {
+      const router = createMemoryRouter([{ path: '/releaseOptions', element: <ReleaseOptions /> }], {
+        initialEntries: ['/releaseOptions?id=123&groups=option_1,option_2&type=xlsx']
+      });
+      render(<RouterProvider router={router} />);
     });
-    render(<RouterProvider router={router} />);
-    ['Option 1', 'Option 2'].forEach((option, indx) => {
-      const optionCheckBox = screen.getAllByRole('checkbox')[indx];
-      expect(optionCheckBox).toBeChecked();
-    });
-    const option3 = screen.getAllByRole('checkbox')[2];
-    expect(option3).not.toBeChecked();
+    waitFor(() => {
+      ['Option 1', 'Option 2'].forEach((option, indx) => {
+        const optionCheckBox = screen.getAllByRole('checkbox')[indx];
+        expect(optionCheckBox).toBeChecked();
+      });
+      const option3 = screen.getAllByRole('checkbox')[2];
+      expect(option3).not.toBeChecked();
 
-    expect(screen.getByTestId('excel-file')).toBeChecked();
+      expect(screen.getByTestId('excel-file')).toBeChecked();
+    });
   });
   it('calls navigate function with updated url when user updates release file columns', async () => {
     const router = createMemoryRouter([{ path: '/releaseOptions', element: <ReleaseOptions /> }], {
@@ -56,7 +62,7 @@ describe('ReleaseOptions', () => {
       const option1 = screen.getAllByRole('checkbox')[0];
       fireEvent.click(option1);
     });
-    expect(navigateFunction).toHaveBeenLastCalledWith('/releaseOptions?id=123&groups=option_1&type=tsv', {
+    expect(navigateFunction).toHaveBeenLastCalledWith('/releaseOptions?id=123&groups=histology&type=tsv', {
       replace: true
     });
   });
@@ -80,8 +86,11 @@ describe('ReleaseOptions', () => {
       const option1 = screen.getAllByRole('checkbox')[0];
       fireEvent.click(option1);
     });
-    expect(navigateFunction).toHaveBeenLastCalledWith('/releaseOptions?id=123&groups=option_2&type=xlsx', {
-      replace: true
-    });
+    expect(navigateFunction).toHaveBeenLastCalledWith(
+      '/releaseOptions?id=123&groups=option_1,option_2,histology&type=xlsx',
+      {
+        replace: true
+      }
+    );
   });
 });
