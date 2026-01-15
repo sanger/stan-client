f0d63bc50116701d996f00a6f568e2490736774e
sabrine33 <sa33@sanger.ac.uk>
2026-01-15 13:56:09 +0000

    fix probe hyb end to end tests

--- src/pages/ProbeHybridisationQC.tsx
+++ src/pages/ProbeHybridisationQC.tsx
@@ -279,7 +279,6 @@ export default function ProbeHybridisationQC() {
                                         await setFieldValue(`hybQCLabware.${index}.workNumber`, workNumber);
                                       }}
                                     />
-                                    {/*<FormikErrorMessage name={`hybQCLabware.${index}.workNumber`} />*/}
                                   </div>
                                   <div>
                                     <FormikInput
