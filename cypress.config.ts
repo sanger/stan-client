1f333c33729c3b1f31bc59341aa13a8b25431538
yoldas <ay6@sanger.ac.uk>
2026-06-27 00:51:52 +0100

    ci: parallelise cypress tests across 4 matrix shards

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+import cypressSplit from 'cypress-split';
 
 export default defineConfig({
   video: false,
@@ -7,10 +8,10 @@ export default defineConfig({
   numTestsKeptInMemory: 0,
   experimentalMemoryManagement: true,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      cypressSplit(on, config);
+      require('./cypress/plugins/index.js')(on, config);
+      return config;
     },
     baseUrl: "http://localhost:3000",
     testIsolation: false,
