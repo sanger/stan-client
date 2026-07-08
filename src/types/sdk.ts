ab828df873c9aab0ed7674023cf2020592f6c95a
sabrine33 <sa33@sanger.ac.uk>
2026-07-08 11:00:47 +0100

    Add xenium study id to the table in 'xenium analyser' page

--- src/types/sdk.ts
+++ src/types/sdk.ts
@@ -43,6 +43,14 @@ export type AddExternalIdRequest = {
   labwareBarcode: Scalars['String']['input'];
 };
 
+/** Request to assign external names to tissues at particular positions in labware. */
+export type AddExternalIdsRequest = {
+  /** The slot addresses and new external names. */
+  addressNames: Array<AddressExternalName>;
+  /** The barcode of the labware containing the tissue */
+  labwareBarcode: Scalars['String']['input'];
+};
+
 /** Request to add spatial locations to an existing tissue type. */
 export type AddSpatialLocationsRequest = {
   /** The name of an existing tissue type. */
@@ -75,6 +83,14 @@ export type AddressCommentInput = {
   address: Scalars['Address']['input'];
   /** The id of a comment in the database. */
   commentId: Scalars['Int']['input'];
+};
+
+/** A link between a slot address and an external name. */
+export type AddressExternalName = {
+  /** A slot address. */
+  address: Scalars['Address']['input'];
+  /** An external name. */
+  externalName: Scalars['String']['input'];
 };
 
 /** Permeabilisation data about a particular slot address. */
@@ -156,8 +172,8 @@ export type AnalyserScanData = {
   cellSegmentationRecorded: Scalars['Boolean']['output'];
   /** The names of probes recorded on the labware. */
   probes: Array<Scalars['String']['output']>;
-  /** The work numbers linked to the labware. */
-  workNumbers: Array<Scalars['String']['output']>;
+  /** The work numbers and xenium study ids linked to the labware. */
+  workNumberXeniumStudyIds: Array<WorkNumberXeniumStudyId>;
 };
 
 /** Biological risk assessment number. */
@@ -940,6 +956,8 @@ export type Mutation = {
   addEquipment: Equipment;
   /** Record an operation adding a external ID to a sample. */
   addExternalID: OperationResult;
+  /** Record an op adding external names to tissues in a particular piece of labware. */
+  addExternalIds: OperationResult;
   /** Create a new fixative that can be selected when registering tissue. */
   addFixative: Fixative;
   /** Create a new HMDMC that can be used when registering new tissue. */
@@ -1215,6 +1233,15 @@ export type MutationAddEquipmentArgs = {
  */
 export type MutationAddExternalIdArgs = {
   request: AddExternalIdRequest;
+};
+
+
+/**
+ * Send information to the application.
+ * These typically require a user with the suitable permission for the particular request.
+ */
+export type MutationAddExternalIdsArgs = {
+  request: AddExternalIdsRequest;
 };
 
 
@@ -2311,6 +2338,8 @@ export type OriginalSampleData = {
   labwareType: Scalars['String']['input'];
   /** The life stage of the donor. */
   lifeStage?: InputMaybe<LifeStage>;
+  /** The pot number used to distinguish pots in a request. */
+  potNumber?: InputMaybe<Scalars['Int']['input']>;
   /** The string to use for the replicate number of the tissue (optional). */
   replicateNumber?: InputMaybe<Scalars['String']['input']>;
   /** The date the original sample was collected, if known. */
@@ -4392,6 +4421,15 @@ export type Work = {
   xeniumStudy?: Maybe<DnapStudy>;
 };
 
+/** A work number and a possible xenium study sequencescape id. */
+export type WorkNumberXeniumStudyId = {
+  __typename?: 'WorkNumberXeniumStudyId';
+  /** A work number. */
+  workNumber: Scalars['String']['output'];
+  /** The sequencescape ID of a xenium study, if the work is linked to one. */
+  xeniumStudyId?: Maybe<Scalars['Int']['output']>;
+};
+
 /** The things that have happened for a particular work, as requested by a user. */
 export type WorkProgress = {
   __typename?: 'WorkProgress';
@@ -4477,7 +4515,7 @@ export type ActionFieldsFragment = { __typename?: 'Action', operationId: number,
 
 export type AddressPermDataFieldsFragment = { __typename?: 'AddressPermData', address: string, controlType?: ControlType | null, seconds?: number | null, selected: boolean };
 
-export type AnalyserScanDataFieldsFragment = { __typename?: 'AnalyserScanData', workNumbers: Array<string>, probes: Array<string>, cellSegmentationRecorded: boolean };
+export type AnalyserScanDataFieldsFragment = { __typename?: 'AnalyserScanData', probes: Array<string>, cellSegmentationRecorded: boolean, workNumberXeniumStudyIds: Array<{ __typename?: 'WorkNumberXeniumStudyId', workNumber: string, xeniumStudyId?: number | null }> };
 
 export type BioRiskFieldsFragment = { __typename?: 'BioRisk', code: string, enabled: boolean };
 
@@ -5633,7 +5671,7 @@ export type GetAnalyserScanDataQueryVariables = Exact<{
 }>;
 
 
-export type GetAnalyserScanDataQuery = { __typename?: 'Query', analyserScanData: { __typename?: 'AnalyserScanData', workNumbers: Array<string>, probes: Array<string>, cellSegmentationRecorded: boolean } };
+export type GetAnalyserScanDataQuery = { __typename?: 'Query', analyserScanData: { __typename?: 'AnalyserScanData', probes: Array<string>, cellSegmentationRecorded: boolean, workNumberXeniumStudyIds: Array<{ __typename?: 'WorkNumberXeniumStudyId', workNumber: string, xeniumStudyId?: number | null }> } };
 
 export type GetBioRisksQueryVariables = Exact<{
   includeDisabled?: InputMaybe<Scalars['Boolean']['input']>;
@@ -5971,7 +6009,10 @@ export const AddressPermDataFieldsFragmentDoc = gql`
     `;
 export const AnalyserScanDataFieldsFragmentDoc = gql`
     fragment AnalyserScanDataFields on AnalyserScanData {
-  workNumbers
+  workNumberXeniumStudyIds {
+    workNumber
+    xeniumStudyId
+  }
   probes
   cellSegmentationRecorded
 }
